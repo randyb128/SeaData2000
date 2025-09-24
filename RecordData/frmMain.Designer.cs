3b56a4149962df20dc905f6ab76afea3d4bf3fd7
Randy Bolton <randyb128@gmail.com>
2025-09-24 08:17:57 -0700

    Add files via upload

---
+++ RecordData/frmMain.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace RecordData
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grpSettings = new System.Windows.Forms.GroupBox();
+            this.txtAvgs = new System.Windows.Forms.TextBox();
+            this.lblAvgs = new System.Windows.Forms.Label();
+            this.btnSaveReadings = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.cboCaptureType = new System.Windows.Forms.ComboBox();
+            this.lblCaptureType = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lblReadings = new System.Windows.Forms.Label();
+            this.txtReadings = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.grpSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grpSettings);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 143;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // grpSettings
+            // 
+            this.grpSettings.Controls.Add(this.txtAvgs);
+            this.grpSettings.Controls.Add(this.lblAvgs);
+            this.grpSettings.Controls.Add(this.btnSaveReadings);
+            this.grpSettings.Controls.Add(this.btnDelete);
+            this.grpSettings.Controls.Add(this.btnCapture);
+            this.grpSettings.Controls.Add(this.cboCaptureType);
+            this.grpSettings.Controls.Add(this.lblCaptureType);
+            this.grpSettings.Controls.Add(this.btnRefresh);
+            this.grpSettings.Controls.Add(this.lblPort);
+            this.grpSettings.Controls.Add(this.btnClose);
+            this.grpSettings.Controls.Add(this.btnOpen);
+            this.grpSettings.Controls.Add(this.cboPort);
+            this.grpSettings.Location = new System.Drawing.Point(15, 12);
+            this.grpSettings.Name = "grpSettings";
+            this.grpSettings.Size = new System.Drawing.Size(782, 116);
+            this.grpSettings.TabIndex = 23;
+            this.grpSettings.TabStop = false;
+            this.grpSettings.Text = " Settings";
+            // 
+            // txtAvgs
+            // 
+            this.txtAvgs.Location = new System.Drawing.Point(351, 80);
+            this.txtAvgs.Name = "txtAvgs";
+            this.txtAvgs.Size = new System.Drawing.Size(100, 20);
+            this.txtAvgs.TabIndex = 18;
+            // 
+            // lblAvgs
+            // 
+            this.lblAvgs.AutoSize = true;
+            this.lblAvgs.Location = new System.Drawing.Point(293, 83);
+            this.lblAvgs.Name = "lblAvgs";
+            this.lblAvgs.Size = new System.Drawing.Size(52, 13);
+            this.lblAvgs.TabIndex = 17;
+            this.lblAvgs.Text = "Averages";
+            // 
+            // btnSaveReadings
+            // 
+            this.btnSaveReadings.Location = new System.Drawing.Point(631, 78);
+            this.btnSaveReadings.Name = "btnSaveReadings";
+            this.btnSaveReadings.Size = new System.Drawing.Size(102, 23);
+            this.btnSaveReadings.TabIndex = 16;
+            this.btnSaveReadings.Text = "Save Readings";
+            this.btnSaveReadings.UseVisualStyleBackColor = true;
+            this.btnSaveReadings.Click += new System.EventHandler(this.btnSaveReadings_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(540, 78);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(85, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Remove Last";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Enabled = false;
+            this.btnCapture.Location = new System.Drawing.Point(459, 78);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
+            this.btnCapture.TabIndex = 14;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // cboCaptureType
+            // 
+            this.cboCaptureType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCaptureType.FormattingEnabled = true;
+            this.cboCaptureType.Items.AddRange(new object[] {
+            "Accelerometer",
+            "Magnetometer"});
+            this.cboCaptureType.Location = new System.Drawing.Point(89, 80);
+            this.cboCaptureType.Name = "cboCaptureType";
+            this.cboCaptureType.Size = new System.Drawing.Size(196, 21);
+            this.cboCaptureType.TabIndex = 13;
+            // 
+            // lblCaptureType
+            // 
+            this.lblCaptureType.AutoSize = true;
+            this.lblCaptureType.Location = new System.Drawing.Point(12, 83);
+            this.lblCaptureType.Name = "lblCaptureType";
+            this.lblCaptureType.Size = new System.Drawing.Size(71, 13);
+            this.lblCaptureType.TabIndex = 12;
+            this.lblCaptureType.Text = "Capture Type";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(479, 19);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(55, 24);
+            this.btnRefresh.TabIndex = 11;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(10, 22);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 10;
+            this.lblPort.Text = "Port";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Enabled = false;
+            this.btnClose.Location = new System.Drawing.Point(422, 19);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(51, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(363, 18);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(53, 23);
+            this.btnOpen.TabIndex = 8;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // cboPort
+            // 
+            this.cboPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Location = new System.Drawing.Point(42, 19);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(303, 21);
+            this.cboPort.TabIndex = 7;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lblReadings);
+            this.splitContainer2.Panel1.Controls.Add(this.txtReadings);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.lblOutput);
+            this.splitContainer2.Panel2.Controls.Add(this.txtOutput);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 303);
+            this.splitContainer2.SplitterDistance = 131;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // lblReadings
+            // 
+            this.lblReadings.AutoSize = true;
+            this.lblReadings.Location = new System.Drawing.Point(12, 8);
+            this.lblReadings.Name = "lblReadings";
+            this.lblReadings.Size = new System.Drawing.Size(52, 13);
+            this.lblReadings.TabIndex = 1;
+            this.lblReadings.Text = "Readings";
+            // 
+            // txtReadings
+            // 
+            this.txtReadings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReadings.Location = new System.Drawing.Point(3, 31);
+            this.txtReadings.Multiline = true;
+            this.txtReadings.Name = "txtReadings";
+            this.txtReadings.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReadings.Size = new System.Drawing.Size(797, 100);
+            this.txtReadings.TabIndex = 0;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(12, 12);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(0, 39);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(800, 129);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // serialPort
+            // 
+            this.serialPort.BaudRate = 115200;
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Record Serial Data";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.grpSettings.ResumeLayout(false);
+            this.grpSettings.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label lblReadings;
+        private System.Windows.Forms.TextBox txtReadings;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.GroupBox grpSettings;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.ComboBox cboCaptureType;
+        private System.Windows.Forms.Label lblCaptureType;
+        private System.Windows.Forms.Button btnSaveReadings;
+        private System.Windows.Forms.TextBox txtAvgs;
+        private System.Windows.Forms.Label lblAvgs;
+    }
+}
+
---
+++ RecordData/frmMain.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecordData
+{
+    public partial class frmMain : Form
+    {
+        public delegate void UpdateTextCallback(string text);
+        enum CaptureType
+        {
+            Accelerometer, Magnetometer
+        }
+        private CaptureType CurrentType = CaptureType.Accelerometer;
+        private bool CaptureFlag = false;
+        private XYZ Totals = new XYZ();
+        private int CurrentAvgs = 25;
+        private List<int> CaptureValueY = new List<int>();
+        private List<int> CaptureValueZ = new List<int>();
+        private string LastLine = string.Empty;
+        private List<string> Lines = new List<string>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        static public List<string> myGetSerialPortNames()
+        {
+            List<string> rc = new List<string>();
+            try
+            {
+                using (ManagementObjectSearcher ManObjSearch = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity WHERE Caption LIKE '%(COM%'"))
+                {
+                    ManagementObjectCollection ManObjReturn = ManObjSearch.Get();
+                    foreach (ManagementObject ManObj in ManObjReturn)
+                    {
+                        string desc = ManObj["Caption"].ToString();
+                        {
+                            rc.Add(desc);
+                        }
+                    }
+                }
+            }
+            catch { }
+            return rc;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            cboPort.DataSource = myGetSerialPortNames();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // refresh list of COM ports
+            btnRefresh_Click(null, null);
+            cboCaptureType.SelectedIndex = 0;
+        }
+
+        private void EnableAll(bool en)
+        {
+            btnOpen.Enabled = !en;
+            btnClose.Enabled = en;
+            btnRefresh.Enabled = !en;
+            cboPort.Enabled = !en;
+            btnDelete.Enabled = en;
+            btnCapture.Enabled = en;
+            cboCaptureType.Enabled = !en;
+            btnDelete.Enabled = en;
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string portName = cboPort.Text.Substring(cboPort.Text.IndexOf("(COM") + 1);
+            portName = portName.Substring(0, portName.IndexOf(')'));
+            try
+            {
+                serialPort.PortName = portName;
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            if (serialPort.IsOpen)
+            {
+                EnableAll(true);
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.Close();
+                }
+                catch { }
+            }
+            EnableAll(false);
+        }
+
+        private void ScrollToEnd(TextBox txt)
+        {
+            txt.SelectionStart = txt.Text.Length;
+            txt.ScrollToCaret();
+            txt.Refresh();
+        }
+
+        private void UpdateOutput(string text)
+        {
+            // save the lines in an array for capturing
+            foreach (char c in text)
+            {
+                if (c == '\r')
+                {
+                    Lines.Add(LastLine);
+                    LastLine = string.Empty;
+                } 
+                else if (c != '\n')
+                {
+                    LastLine += c;
+                }
+                
+            }
+            txtOutput.AppendText(text);
+            ScrollToEnd(txtOutput);
+        }
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string s = serialPort.ReadExisting();
+
+            txtOutput.Invoke(new UpdateTextCallback(this.UpdateOutput), s);
+            if (CaptureFlag)
+            {
+                if (Lines.Count > 0)
+                {
+                    if (CurrentType == CaptureType.Accelerometer)
+                    {
+                        foreach(string str in Lines)
+                        {
+                            if (str.StartsWith("a:\t"))
+                            {
+                                Totals.Add(str);
+                                if (Totals.Count >= CurrentAvgs)     // capture X readings for the average
+                                {
+                                    txtReadings.AppendText(Totals.Average());
+                                    Totals.Clear();
+                                    CaptureFlag = false;
+                                    btnCapture.Enabled = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    Lines.Clear();
+                }
+            }
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            CurrentType = (CaptureType)cboCaptureType.SelectedIndex;
+            if (CurrentType == CaptureType.Accelerometer)
+            {
+                CurrentAvgs = atoi(txtAvgs.Text);
+                if (CurrentAvgs <= 0 || CurrentAvgs > 99)
+                    CurrentAvgs = 25;
+            }
+            Totals.Clear();
+            Lines.Clear();
+            CaptureFlag = true;
+
+            btnCapture.Enabled = false;
+        }
+
+        /// <summary>
+        /// Helper function convert string to int
+        /// </summary>
+        /// <param name="s">string to convert</param>
+        /// <returns>integer value else zero if error</returns>
+        public static int atoi(string s)
+        {
+            int i = 0;
+            int.TryParse(s, out i);
+            return i;
+        }
+
+
+        public class XYZ
+        {
+            public XYZ()
+            {
+                Clear();
+            }
+
+            public void Clear()
+            {
+                X = 0;
+                Y = 0;
+                Z = 0;
+                Count = 0;
+            }
+
+            public int Count { get; set; }
+
+            public void Add(string data)
+            {
+                string[] strs = data.Split('\t');
+                if (strs.Length == 4)
+                {
+                    X += atoi(strs[1]);
+                    Y += atoi(strs[2]);
+                    Z += atoi(strs[3]);
+                }
+                Count++;
+            }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+
+            public string Average()
+            {
+                return string.Format("{0}\t{1}\t{2}\r\n", X / Count, Y / Count, Z / Count);
+            }
+        }
+
+        private void btnSaveReadings_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string fileName = string.Format(@"{0}_{1}_{2}.txt", (CaptureType)cboCaptureType.SelectedIndex == CaptureType.Accelerometer ? "Accel" : "Mag", now.ToString("yyyyMMdd"), now.ToString("HHmmss"));
+            bool fileErr = false;
+            try
+            {
+                if (txtReadings.Lines.Length == 0)
+                    MessageBox.Show("No data to be saved");
+                else
+                    using (StreamWriter sw = new StreamWriter(fileName))
+                    {
+                        foreach(string line in txtReadings.Lines)
+                            sw.WriteLine(line);
+                    }
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message);
+                fileErr = true;
+            }
+            if (!fileErr)
+                MessageBox.Show(string.Format("Readings saved to {0}", fileName));
+        }
+    }
+}
