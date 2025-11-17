36f685c0405ccd14c4e29255d1b76c0e7cf74ef1
Randy <randyb128@gmail.com>
2025-11-17 17:52:03 -0500

    Create GPSSerialBridge.ino

---
+++ Examples/GPS/GPSSerialBridge/GPSSerialBridge.ino
@@ -0,0 +1,27 @@
+/*
+ use this to communicate with the GPS Neo-8M module via UBlox u-center via Serial1
+*/
+
+// pins for NEO-8M GPS Module
+static const int RXPin = 21, TXPin = 22;
+
+#define COMPUTER_BAUDRATE 115200
+#define GPS_BAUDRATE 9600
+
+void setup() {
+  Serial.begin(COMPUTER_BAUDRATE);
+  Serial1.begin(GPS_BAUDRATE, SERIAL_8N1, RXPin, TXPin);
+}
+
+void loop() {
+    // If there is data from the GPS receiver, read it and send it to the computer or vice versa.
+    if (Serial1.available())
+    {
+        Serial.write(Serial1.read());
+    }
+
+    if (Serial.available())
+    {
+        Serial1.write(Serial.read());
+    }
+}
