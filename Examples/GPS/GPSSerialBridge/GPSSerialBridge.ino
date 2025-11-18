949936779c599e847ad77595c1b374b04c5f0c33
Randy <randyb128@gmail.com>
2025-11-18 15:35:26 -0500

    Original push

--- Examples/GPS/GPSSerialBridge/GPSSerialBridge.ino
+++ Examples/GPS/GPSSerialBridge/GPSSerialBridge.ino
@@ -1,11 +1,13 @@
 /*
- use this to communicate with the GPS Neo-8M module via UBlox u-center via Serial1
+ use this to communicate with the GPS Neo-8M module via UBlox u-center
+ You can also see the data on the Serial Console of the Arduino IDE provided the device is in NMEA 0183 output mode
 */
 
 // pins for NEO-8M GPS Module
 static const int RXPin = 21, TXPin = 22;
 
 #define COMPUTER_BAUDRATE 115200
+// if you've used the Neo8MConfig program and have set the protocol to UBX then you'll need to change the GPS_BAUDRATE to 115200
 #define GPS_BAUDRATE 9600
 
 void setup() {
--- Examples/GPS/Neo8MMQTT/Neo8MMQTT.ino
+++ Examples/GPS/Neo8MMQTT/Neo8MMQTT.ino
@@ -1,6 +1,27 @@
+// Ublox GPS Neo8M MQTT Example  rb July 8,2025
+// This example expects that the NEO8M GPS has been configured using the Neo8mConfig example
+// the Neo8m should be set to 115k baud with UBX output 10 times per second
+// 
+
+
 #include <UbxGpsNavPvt8.h>
 #include <UbxGpsNavSat.h>
-#include <BLEParams.h>
+#include <PicoMQTT.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <time.h>
+
+// uncomment to create a WiFi access point in which you can connect your phone's WiFi
+// otherwise, we'll connect to an existing WiFi access point
+//#define WIFI_ACCESS_POINT
+
+#ifndef WIFI_SSID
+#define WIFI_SSID "TheMudgkins"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "1mudgkins2"
+#endif
 
 // pins for NEO GPS Module
 static const int RXPin = 4, TXPin = 5;
@@ -13,65 +34,73 @@ static const int RXPin = 4, TXPin = 5;
 
 UbxGpsNavPvt8 pvt;
 UbxGpsNavSat sat;
-
-BLEParams pserv;
+PicoMQTT::Server mqtt;
 unsigned long lastTime;
-
+const char *fixType[] = { "0: no fix", "1: dead reckoning only", "2: 2D-fix", "3: 3D-fix", "4: GNSS + dead reckoning combined", "5: time only fix", "Unknown" };
 char datetime[DATETIME_LENGTH];
-
-enum MonitorNdx { LON, LAT, SPEED, SATS };
 
 void setup() {
   Serial.begin(COMPUTER_BAUDRATE);
-  Serial1.begin(GPS_BAUDRATE, SERIAL_8N1, RXPin, TXPin);
-  pserv.AddToMonitor(LON, "Lon", 0.0, 7);
-  pserv.AddToMonitor(LAT, "Lat", 0.0, 7);
-  pserv.AddToMonitor(SPEED, INT_V, "Speed", 0);
-  pserv.AddToMonitor(SATS, INT_V, "Sats", 0);
-  pserv.begin(String("SeaData"));
+  Serial1.begin(GPS_BAUDRATE, SERIAL_8N1, RXPin, TXPin);    // 2nd serial port for Neo8m module
+  Wire.begin();   // start i2c bus
+
+#ifdef WIFI_ACCESS_POINT
+  // Create WiFi Access Point
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+  delay(2000);
+  IPAddress ip = WiFi.softAPIP();
+  Serial.print("WiFi server IP: ");
+  Serial.println(ip);
+#else
+  // Connect to existing WiFi Access Point
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) { delay(1000); }
+  delay(2000);
+  Serial.printf("WiFi connected, IP: %s\n", WiFi.localIP().toString().c_str());
+#endif
+  mqtt.begin();
+
   lastTime = millis();
 }
 
 void loop() {
   unsigned char b;
+
+  mqtt.loop();
   // put your main code here, to run repeatedly:
   while(Serial1.available())
   {
+    mqtt.loop();
     b = Serial1.read();
     if (pvt.ready(b))
     {
         if (millis() - lastTime >= 1000)
         {
-        // update the phone app values once/second
+          // update the MQTT values for the phone app once/second
           lastTime = millis();
           snprintf(datetime, DATETIME_LENGTH, DATETIME_FORMAT, pvt.NavPvt8.year, pvt.NavPvt8.month, pvt.NavPvt8.day, pvt.NavPvt8.hour, pvt.NavPvt8.min, pvt.NavPvt8.sec);
-
-          Serial.print(datetime);
-          Serial.print(',');
+          mqtt.publish(String("gps/date"), datetime);
+          Serial.print("GPS Date: ");
+          Serial.println(datetime);
           double lon = pvt.NavPvt8.lon / 10000000.0;
-          Serial.print(lon, 7);
-          Serial.print(',');
+          mqtt.publish(String("gps/longitude"), String(lon, 5));
           double lat = pvt.NavPvt8.lat / 10000000.0;
-          Serial.print(lat, 7);
-          Serial.print(',');
-          Serial.print(pvt.NavPvt8.height / 1000.0, 3);
-          Serial.print(',');
-          Serial.print(pvt.NavPvt8.hMSL / 1000.0, 3);
-          Serial.print(',');
-          double dspeed = pvt.NavPvt8.gSpeed * 0.0036;
-          Serial.print(dspeed, 5);
-          Serial.print(',');
-          Serial.print(pvt.NavPvt8.headMot / 100000.0, 5);
-          Serial.print(',');
-          Serial.print(pvt.NavPvt8.fixType);
-          Serial.print(',');
-          Serial.print(pvt.NavPvt8.pDOP / 100.0, 2);
-          Serial.print(',');
-          Serial.println(pvt.NavPvt8.numSV);
-          pserv.SetMonitorValue(LON, lon);
-          pserv.SetMonitorValue(LAT, lat);
-          pserv.SetMonitorValue(SPEED, (int)dspeed);
-          pserv.SetMonitorValue(SATS, (int)pvt.NavPvt8.numSV);
+          mqtt.publish(String("gps/latitude"), String(lat, 5));
+          //Serial.print(pvt.NavPvt8.hMSL / 1000.0, 3);
+          // speed over ground is in mm/s but we want knots
+          double dspeed = pvt.NavPvt8.gSpeed * 0.00194384;
+          mqtt.publish(String("gps/sog"), String(dspeed, 1));
+          double headMot = pvt.NavPvt8.headMot / 100000.0;
+          mqtt.publish(String("gps/cog"), String(headMot, 1));
+          int fixndx = pvt.NavPvt8.fixType;
+          if (fixndx >= sizeof(fixType))
+            fixndx = sizeof(fixType) - 1;
+          mqtt.publish(String("gps/fix"), String(fixType[fixndx]));
+          double pdop = pvt.NavPvt8.pDOP / 100.0;
+          mqtt.publish(String("gps/pdop"), String(pdop, 2));
+          mqtt.publish(String("gps/numsv"), String(pvt.NavPvt8.numSV));
         }
     }
     if (sat.ready(b))
@@ -87,7 +116,6 @@ void loop() {
         }
         Serial.println();
     }
-    pserv.loop();   // keep web server running
   }
 
 }
